b341d6d3590aca846cd2803500635edfcef9663f
jvrasmatas <juanvrasmatas@gmail.com>
2026-06-01 14:25:01 -0400

    habilitar contador - logica de sumar, restar, resetear

--- contador_app/flutter_application_1/lib/screens/home_screen.dart
+++ contador_app/flutter_application_1/lib/screens/home_screen.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
 
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int _valor = 0;
+
+  void sumar() {
+    _valor++;
+    setState(() {});
+  }
+  void restar() {
+    _valor--;
+    setState(() {});
+  }
+  void resetear() {
+    _valor = 0;
+    setState(() {});
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,8 +32,45 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Contador'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('0'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Valor actual',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '$_valor',
+              style: const TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 'sumar',
+            onPressed: sumar,
+            backgroundColor: Color.fromARGB(255, 25, 34, 98),
+            child: const Icon(Icons.add, color: Colors.white),
+          ),
+          const SizedBox(height: 12),
+          FloatingActionButton(
+            heroTag: 'restar',
+            onPressed: restar,
+            backgroundColor: const Color.fromARGB(255, 25, 34, 98),
+            child: const Icon(Icons.remove, color: Colors.white),
+          ),
+          const SizedBox(height: 12),
+          FloatingActionButton(
+            heroTag: 'resetear',
+            onPressed: resetear,
+            backgroundColor: Color.fromARGB(255, 25, 34, 98),
+            child: const Icon(Icons.refresh_rounded, color: Colors.white),
+          ),
+        ],
       ),
     );
   }
